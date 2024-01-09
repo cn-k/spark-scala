e99570383c99fa4318775299508e6b7e2d37e728
cakdeniz <cenk.akdeniz@asml.com>
2024-01-09 20:33:37 +0100

    add

--- src/main/scala/WalmartTransactionDataAnalyzer.scala
+++ src/main/scala/WalmartTransactionDataAnalyzer.scala
@@ -1,14 +1,14 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 
 import scala.concurrent.duration._
 
 object WalmartTransactionDataAnalyzer {
 
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "")
+    //System.setProperty("hadoop.home.dir", "")
     val spark = SparkSession
       .builder()
       .master("local[*]")
@@ -23,27 +23,33 @@ object WalmartTransactionDataAnalyzer {
         StructField("ProductID", IntegerType, nullable = true),
         StructField("Quantity", IntegerType, nullable = true),
         StructField("UnitPrice", IntegerType, nullable = true),
-        StructField("TotalPrice", IntegerType, nullable = true)
+        StructField("TotalPrice", IntegerType, nullable = true),
+        StructField("Timestamp", TimestampType, nullable = true)
       )
     )
 
     // Read stream from a directory as if new files were being added continuously
     val transactionData = spark.readStream
-      .format("csv")
-      .option("header", "true")
-      .schema(schema)
-      .load("file:///C:/Users/cnk_a/Desktop/projects/java/spark-scala/source/*.csv")
-
+      .format("socket")
+      .option("host","localhost")
+      .option("port","9000")
+      .load()
+//      .format("csv")
+//      .option("header", "true")
+//      .schema(schema)
+//      .load("file:///C:/Users/cnk_a/Desktop/projects/java/spark-scala/source/*.csv")
+    /*
     val windowedCounts = transactionData
-      .groupBy(window($"timestamp", "10 seconds"), $"productID")
+      .groupBy(window($"Timestamp", "10 seconds"), $"productID")
       .agg(sum("TotalPrice").as("revenue"))
-
+    */
     // You may further process windowedCounts as needed for analysis
+    val windowedCounts = transactionData.select("ProductID","UnitPrice").where("ProductID =45678")
 
     val query = windowedCounts.writeStream
       .outputMode("complete") // Specify the output mode as needed (e.g., complete, append, update)
       .format("console") // Output to console for example purposes
-      .trigger(Trigger.ProcessingTime(5.minutes)) // Trigger interval
+      //.trigger(Trigger.ProcessingTime(5.seconds)) // Trigger interval
       .start()
 
     query.awaitTermination() // Keep the streaming query running indefinitely
